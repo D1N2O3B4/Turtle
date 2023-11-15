c133df07cdf754ab30099601b8879c6a41755223
d1n2o3b4 <davebostons2017@gmail.com>
2023-11-15 19:31:10 +0000

    made changes

--- counts.py
+++ counts.py
@@ -1,4 +1,13 @@
-from collections import Counter
+from collections import Counter, namedtuple, defaultdict, OrderedDict,deque
 
 a = "122333444455555"
 print(Counter(a))
+
+b = Counter([3,2,2,2,2,2,4,4,4,1,7,7,9]).most_common()
+print(b)
+
+#namedtuple
+
+Human = namedtuple('Human',['name','age','height'])
+d = Human(name="David",age="22",height=187)
+print(d)
