9d854dba60efb309a0c175877564764605b5fa83
d1n2o3b4 <davebostons2017@gmail.com>
2023-11-30 03:45:37 +0000

    added trycatch.py

---
+++ trycatch.py
@@ -0,0 +1,11 @@
+import sys
+# choice = input("Type sth: ")
+
+# try:
+#     ans = eval(choice)+233
+# except TypeError:
+#     print("Type error")
+# except:
+#     print("Could not evaluate")
+    
+print(len(sys.argv))
